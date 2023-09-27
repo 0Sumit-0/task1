778bc80329662eedadf0169c325c695951a7cf65
Sumit <sumitanwar08@gmail.com>
2023-09-27 06:41:44 +0530

    initial commit 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:interviewtask/pages/signup.dart';
-import 'package:interviewtask/pages/signup1.dart';
 
 void main() {
   runApp(const MyApp());
